2869a23f0fdf21119c0d12b7f343f0fab80802ec
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-02-28 13:42:28 +0100

    istanze-metodi

--- java-oop-shop-base/src/org/lessons/java/shop/Prodotto.java
+++ java-oop-shop-base/src/org/lessons/java/shop/Prodotto.java
@@ -1,5 +1,44 @@
 package org.lessons.java.shop;
 
-public class Prodotto {
+//random
+import java.util.Random;
 
+public class Prodotto {
+    // ISTANZE
+    public int codice;
+    public String nome;
+    public String descrizione;
+    public float prezzo;
+    public int iva;
+
+    // METODI
+    // costruttore
+    public Prodotto(String nome, String descrizione, float prezzo, int iva) {
+        this.nome = nome;
+        this.descrizione = descrizione;
+        this.prezzo = prezzo;
+        this.iva = iva;
+    }
+
+    // metodo per codice random
+    public void codiceRandom() {
+        Random random = new Random();
+        int max = 101;
+        this.codice = random.nextInt(max);
+    }
+
+    // metodo per prezzo base
+    public void prezzoBase(float prezzo) {
+        this.prezzo = prezzo;
+    }
+
+    // metodo per prezzo comprensivo di iva
+    public void prezzoIvato(int prezzo, int iva) {
+        this.prezzo = prezzo + (prezzo * iva / 100);
+    }
+
+    // metodo per nome esteso concatenando codice e nome
+    public void codicePiuNome(int codice, String nome) {
+        this.nome = codice + "-" + nome;
+    }
 }
