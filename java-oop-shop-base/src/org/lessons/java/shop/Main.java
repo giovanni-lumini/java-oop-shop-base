63eda245aff73c4695ec76c5dc6bc1d4ff4d8aab
giovanni-lumini <giovanni.lumini28@gmail.com>
2025-03-03 12:13:55 +0100

    correction

--- java-oop-shop-base/src/org/lessons/java/shop/Main.java
+++ java-oop-shop-base/src/org/lessons/java/shop/Main.java
@@ -1,27 +1,24 @@
 package org.lessons.java.shop;
 
+//decimali per valiute e arrotondamenti (per operazioni si usano metodi e non /, *, +, -)
+import java.math.BigDecimal;
+
 public class Main {
     public static void main(String[] args) {
         // creo un nuovo oggetto prodotto
 
         // nuovo prodotto
-        Prodotto nuovoProdotto = new Prodotto("iphone", "iphone 16 pro max", 22);
-
-        // metodo per codice random
-        nuovoProdotto.codiceRandom();
-        System.out.println(nuovoProdotto.codice);
+        Prodotto nuovoProdotto = new Prodotto("iphone 16 pro max", "iphone di ultima generazione", new BigDecimal(999),
+                new BigDecimal(0.22));
 
         // metodo per prezzo base
-        nuovoProdotto.prezzoBase();
-        System.out.println(nuovoProdotto.prezzo);
+        System.out.println(nuovoProdotto.getPrezzoBase());
 
         // metodo per prezzo comprensivo di iva
-        nuovoProdotto.prezzoIvato(nuovoProdotto.prezzo, nuovoProdotto.iva);
-        System.out.println(nuovoProdotto.prezzo);
+        System.out.println(nuovoProdotto.getPrezzoIvato());
 
         // metodo per nome esteso concatenando codice e nome
-        nuovoProdotto.codicePiuNome(nuovoProdotto.codice, nuovoProdotto.nome);
-        System.out.println(nuovoProdotto.nome);
+        System.out.println(nuovoProdotto.getNomeEsteso());
 
     }
 }
--- java-oop-shop-base/src/org/lessons/java/shop/Prodotto.java
+++ java-oop-shop-base/src/org/lessons/java/shop/Prodotto.java
@@ -3,41 +3,54 @@ package org.lessons.java.shop;
 //random
 import java.util.Random;
 
+//decimali per valiute e arrotondamenti (per operazioni si usano metodi e non /, *, +, -)
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 public class Prodotto {
     // ISTANZE
     public int codice;
     public String nome;
     public String descrizione;
-    public float prezzo;
-    public int iva;
+    // BigDecimal va bene per le valute
+    public BigDecimal prezzo;
+    public BigDecimal iva;
 
     // METODI
     // costruttore
-    public Prodotto(String nome, String descrizione, int iva) {
+    public Prodotto(String nome, String descrizione, BigDecimal prezzo, BigDecimal iva) {
+        // metodo per codice random
+        Random random = new Random();
+        this.codice = random.nextInt(99999);
+
         this.nome = nome;
         this.descrizione = descrizione;
+        this.prezzo = prezzo;
         this.iva = iva;
     }
 
-    // metodo per codice random
-    public void codiceRandom() {
-        Random random = new Random();
-        int max = 101;
-        this.codice = random.nextInt(max);
-    }
-
     // metodo per prezzo base
-    public void prezzoBase() {
-        this.prezzo = 999.99f;
+    // i metodi get consentono di recuperare una variabile d'istanza (this.prezzo)
+    public BigDecimal getPrezzoBase() {
+        return this.prezzo;
     }
 
     // metodo per prezzo comprensivo di iva
-    public void prezzoIvato(float prezzo, int iva) {
-        this.prezzo = prezzo + (prezzo * iva / 100);
+    public BigDecimal getPrezzoIvato() {
+        if (prezzo != null && iva != null) {
+            // per i BigDecimal per le operazioni si usa: add, multiply ecc..
+            // setScale permette di definire il numero di cifre decimali e se arrotondare
+            // verso l'alto o il basso
+            return prezzo.add(prezzo.multiply(iva).setScale(2, RoundingMode.DOWN));
+        }
+        return null;
     }
 
     // metodo per nome esteso concatenando codice e nome
-    public void codicePiuNome(int codice, String nome) {
-        this.nome = codice + "-" + nome;
+    public String getNomeEsteso() {
+        if (nome != null) {
+            return codice + "-" + nome;
+        }
+        return null;
     }
 }
